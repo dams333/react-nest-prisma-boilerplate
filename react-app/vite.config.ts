cf11a40fdae14f7f694e43d51ea37b6da348fe56
Damien <hubleur.damien333@protonmail.ch>
2023-08-04 18:01:14 +0200

    init commit

---
+++ react-app/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	server: {
+		host: true,
+		port: 8080,
+	},
+	plugins: [
+		react(),
+		eslint({
+			cache: false,
+			include: ['./src/**/*.ts', './src/**/*.tsx'],
+			exclude: [],
+			lintOnStart: true,
+		}),
+	],
+	resolve: {
+		alias: [{ find: '@', replacement: '/src' }],
+	},
+});
